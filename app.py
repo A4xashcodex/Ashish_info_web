b4247aceb7888e90e008bc8b96db6fd3bb072c85
A4xashcodex <famousashish95077@gmail.com>
2026-09-12 21:03:51 +0530

    Update API_BASE_URL for player info retrieval

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import random
 app = Flask(__name__)
 
 # Configuration
-API_BASE_URL = "https://infoooooo-v6v5.vercel.app/accinfo"
+API_BASE_URL = "https://player-info-ob54.vercel.app/player-info?uid={uid}"
 ICON_BASE_URL = "https://cdn.jsdelivr.net/gh/ShahGCreator/icon@main/PNG"
 
 def format_timestamp(timestamp):
