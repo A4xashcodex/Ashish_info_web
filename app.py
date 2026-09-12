45df8f0235d31c34b0e1869cd3ea20ad375b0a5f
A4xashcodex <famousashish95077@gmail.com>
2026-09-12 22:13:18 +0530

    Update API_BASE_URL to include query parameters

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import random
 app = Flask(__name__)
 
 # Configuration
-API_BASE_URL = "https://stargamerff.qzz.io/accinfo"
+API_BASE_URL = "https://stargamerff.qzz.io/accinfo?"
 ICON_BASE_URL = "https://cdn.jsdelivr.net/gh/ShahGCreator/icon@main/PNG"
 
 def format_timestamp(timestamp):
